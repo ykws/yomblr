47790d8ba8265758694f57a2c3d3aab36b6c634e
Yoshiyuki Kawashima <ykawashi7@gmail.com>
2017-07-14 17:51:15 +0900

    Set icon

--- yomblr/icon_maker.sh
+++ yomblr/icon_maker.sh
@@ -1,3 +1,22 @@
+#!/bin/sh
+src=$1
+
+sips -Z 120 $src --out Assets.xcassets/AppIcon.appiconset/icon-60@2x.png
+sips -Z 180 $src --out Assets.xcassets/AppIcon.appiconset/icon-60@3x.png
+sips -Z  76 $src --out Assets.xcassets/AppIcon.appiconset/icon-76.png
+sips -Z 152 $src --out Assets.xcassets/AppIcon.appiconset/icon-76@2x.png
+sips -Z 167 $src --out Assets.xcassets/AppIcon.appiconset/icon-83.5@2x.png
+sips -Z  29 $src --out Assets.xcassets/AppIcon.appiconset/icon-29.png
+sips -Z  58 $src --out Assets.xcassets/AppIcon.appiconset/icon-29@2x.png
+sips -Z  87 $src --out Assets.xcassets/AppIcon.appiconset/icon-29@3x.png
+sips -Z  40 $src --out Assets.xcassets/AppIcon.appiconset/icon-40.png
+sips -Z  80 $src --out Assets.xcassets/AppIcon.appiconset/icon-40@2x.png
+sips -Z 120 $src --out Assets.xcassets/AppIcon.appiconset/icon-40@3x.png
+sips -Z  20 $src --out Assets.xcassets/AppIcon.appiconset/icon-20.png
+sips -Z  40 $src --out Assets.xcassets/AppIcon.appiconset/icon-20@2x.png
+sips -Z  60 $src --out Assets.xcassets/AppIcon.appiconset/icon-20@3x.png
+
+cat << EOF > Assets.xcassets/AppIcon.appiconset/Contents.json
 {
   "images" : [
     {
@@ -108,3 +127,4 @@
     "author" : "xcode"
   }
 }
+EOF
